a3ed78e86c892c0623b6f936a2fe98d0ff0bc444
= <=>
2020-08-19 20:34:18 -0300

    fet: add morgan to log

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import "reflect-metadata";
+import morgan from "morgan";
 import { App } from "./app";
 import { routes } from "./routes";
 
@@ -8,5 +9,6 @@ const app = new App(httpProvider);
 
 app.ApplyMiddleware(express.json());
 app.ApplyMiddleware(routes);
+app.ApplyMiddleware(morgan("combined"));
 
 app.Startup().listen(3000);

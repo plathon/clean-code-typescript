2f469fc841c1eac1dc1561a5338fe58b3c581f60
= <=>
2020-08-19 18:49:15 -0300

    update: add data layer to repository in useCase

--- src/useCases/createUser/index.ts
+++ src/useCases/createUser/index.ts
@@ -1,7 +1,9 @@
 import { CreateUserUseCase } from "./CreateUserUseCase";
 import { UserRepository } from "../../repositories/UserRepository";
+import { UserPostgresDataSource } from "../../data/UserPostgresDataSource";
 
-const userRepository = new UserRepository();
+const userPostgresDataSource = new UserPostgresDataSource();
+const userRepository = new UserRepository(userPostgresDataSource);
 const createUserUseCase = new CreateUserUseCase(userRepository);
 
 export { createUserUseCase };

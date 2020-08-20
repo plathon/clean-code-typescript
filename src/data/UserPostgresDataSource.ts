40b71a5781ec57061be3c1d89c7550c2bba6ea2a
= <=>
2020-08-20 13:13:43 -0300

    ref: using async/await in data source

--- src/data/UserPostgresDataSource.ts
+++ src/data/UserPostgresDataSource.ts
@@ -1,18 +1,13 @@
 import { User } from "../entities/User";
 import { connection } from "../config/database";
 import { IUserDataSource } from "./IUserDataSource";
+import { Http400Error } from "../errors/Http400Error";
 
 export class UserPostgresDataSource implements IUserDataSource {
   async FindOrCreate(data: User) {
-    connection
-      .then((con) => {
-        console.log(data);
-        const repo = con.getRepository(User);
-        repo.save(data);
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
-    return data;
+    const database = await connection;
+    const repo = database.getRepository(User);
+    const result = await repo.save(data);
+    return result;
   }
 }

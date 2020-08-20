459307e05e91b20b73c6f70f77bcf0300664ba3b
= <=>
2020-08-20 13:10:17 -0300

    fet: add error handler middleware

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,16 @@ import "reflect-metadata";
 import morgan from "morgan";
 import { App } from "./app";
 import { routes } from "./routes";
+import { ErrorHandler } from "./errors/ErrorHandler";
 
 const httpProvider = express();
 const app = new App(httpProvider);
 
 app.ApplyMiddleware(express.json());
-app.ApplyMiddleware(routes);
 app.ApplyMiddleware(morgan("combined"));
+app.ApplyMiddleware(routes);
+
+const errorHandler = new ErrorHandler();
+app.ApplyMiddleware(errorHandler.handle);
 
 app.Startup().listen(3000);
